b4910752c617a60477f5d5740ce5f971bb88b73d
Thomas Gracie <tgracie030@gmail.com>
2018-09-23 12:05:29 +0100

    Added the drink class and tested all details returning

--- drink.rb
+++ drink.rb
@@ -1,19 +1,32 @@
-# #define the class
+#define the class
+
+class Drink
+
+#######################################################################
+  attr_accessor :name, :brand, :price, :abv
+
+#######################################################################
+  def initialize(name, brand, price, alcohol_content)
+
+    @name = name
+    @brand = brand
+    @price = price
+    @abv = alcohol_content
+
+
+  end
+#######################################################################
+
+end #class end
+
+# stock [
+#   {
+#     drink: @beer
+#     stock: 200
+#   },
 #
-# class Drink
 #
-# #######################################################################
-#   attr_accessor :name
+# ]
+#  @beer =  Drink.new("Original", price, abv)
 #
-# #######################################################################
-#   def initialize(sl)
-#
-#     @name = name
-#     @price = price
-#     @abv = alcohol_content
-#     @stock_level = sl
-#
-#   end
-# #######################################################################
-#
-# end #class end
+#  stock{:drink}.price * stock{:stock}
--- spec/drink_spec.rb
+++ spec/drink_spec.rb
@@ -8,5 +8,13 @@ require_relative("../drink.rb")
 
 class DrinkTest < MiniTest::Test
 
+  def test_drink_details
+    beer = Drink.new("Original", "Innis & Gunn", 4, 7)
+    assert_equal("Original", beer.name)
+    assert_equal("Innis & Gunn", beer.brand)
+    assert_equal(4, beer.price)
+    assert_equal(7, beer.abv)
+  end
+
 
 end #Class/Test end
